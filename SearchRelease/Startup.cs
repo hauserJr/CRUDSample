29abf328ce91b072c5d605c713751c7cfd997755
名宏 鄭 <free576002@gmail.com>
2018-03-09 18:05:36 +0800

    加入專案檔案。

---
+++ SearchRelease/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SearchRelease.Startup))]
+namespace SearchRelease
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
